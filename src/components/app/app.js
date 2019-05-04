5e94d572a19378cade11413235da07a28a2172e6
Ilya Averin <37779678+ilya-averin@users.noreply.github.com>
2019-05-04 17:33:03 +0200

    next commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,30 +8,43 @@ import ItemStatusFilter from '../item-status-filter';
 import './app.css';
 
 
-export default class App extends {
+export default class App extends Component {
 
 	state = {
+    todoData: [
+      { label: 'Learn React.js', important: false, id: 1 },
+      { label: 'Learn Vue.js', important: true, id: 2 },
+      { label: 'Make Awesome app', important: false, id: 3 }
+    ]
+  };
 
-	};
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+			const idx = todoData.findIndex((el) => el.id === id);
+				// const before = todoData.slice(0, idx);
+				// 	const after = todoData.slice(idx );
+							const newArray = [
+								...todoData.slice(0, idx),
+								...todoData.slice(idx +1)
+							];
+					return {
+				todoData: newArray
+			};
+		});
+  };
 
-  const todoData = [
-    { label: 'Learn React.js', important: false, id: 1 },
-    { label: 'Learn Vue.js', important: true, id: 2 },
-    { label: 'Make Awesome app', important: false, id: 3 }
-  ];
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+        <TodoList todos={this.state.todoData}
+          onDeleted = { this.deleteItem }/>
       </div>
-
-      <TodoList todos={todoData}
-				onDeleted = { (id) => console.log('del', id)}/>
-    </div>
-  );
+    );
+  }
 };
-
-export default App;
