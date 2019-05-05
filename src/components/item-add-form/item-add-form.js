2c35f0441e03ee0b9f5bfc59529dbac883649c85
Ilya Averin <37779678+ilya-averin@users.noreply.github.com>
2019-05-06 01:05:48 +0200

    create add tasks

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,12 +4,31 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
+	state = {
+		label: ''
+	};
+
+	onLabelChanges = (e)=> {
+		this.setState({
+			label: e.target.value
+		});
+	};
+
+	onSubmit = (e) => {
+		e.preventDefault();
+			this.props.onItemAdded(this.state.label);
+	};
+
 	render() {
 		return (
-			<div className="item-add-form">
-				<button className="btn btn-outline-secondary"
-				onClick = {() => this.props.onItemAdded('Hello World!!!') }>Add note</button>
-			</div>
+			<form className="item-add-form d-flex"
+				onSubmit = {this.onSubmit}>
+
+				<input type="text" className="form-control"
+						onChange = {this.onLabelChanges} placeholder="What needs to be done" />
+
+				<button className="btn btn-outline-secondary"> Add </button>
+		</form>
 		)
 	}
 }
